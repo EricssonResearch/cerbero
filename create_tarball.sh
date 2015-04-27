5a305f749b97a82836b3da11f5e3731921405127
Robert Swain <robert.swain@ericsson.com>
2015-04-27 08:46:06 +0200

    create_tarball.sh: Use bzip2 and make tarball from HEAD
    'master' is not always present. xz is not always available, bzip2 is.

--- create_tarball.sh
+++ create_tarball.sh
@@ -4,11 +4,11 @@ if [ $# -eq 0 -o $# -gt 1 ]; then
     echo "$0: Creates a tarball of cerbero and cerbero/sources/local"
     echo "Usage:"
     echo "$0 <output-filename>"
-    echo "This results in <output-filename>.tar.xz"
+    echo "This results in <output-filename>.tar.bz2"
     exit 0
 fi
 
 FILENAME=$1
-git archive -v --format=tar --prefix=$FILENAME/cerbero/ master > $FILENAME.tar && \
+git archive -v --format=tar --prefix=$FILENAME/cerbero/ HEAD > $FILENAME.tar && \
     tar rvf $FILENAME.tar -s ",^sources,$FILENAME/cerbero/sources," sources/local/ && \
-    xz -T0 $FILENAME.tar
+    bzip2 $FILENAME.tar
